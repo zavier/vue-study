fb470eb88798b7e288c3d923ae7e3c4ac7b5ad13
zheng <765324639@qq.com>
2018-11-13 00:53:58 +0800

    add axios

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -10,12 +10,25 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import axios from 'axios'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons
+  },
+  methods:{
+    getHomeInfo () {
+      axios.get('/api/index.json')
+      .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      console.log(res)
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
   }
 }
 </script>
