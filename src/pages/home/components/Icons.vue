79ca57a8d045a047e1d7b44de1fbcee67fed03f0
zheng <765324639@qq.com>
2018-11-12 23:23:16 +0800

    add base icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .icons
+    .icon
+      position: relative
+      float: left
+      width: 25%
+      padding-bottom: 25%
+      height 0
+      overflow: hidden
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        box-sizing border-box
+        padding .1rem
+        bottom: .44rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+</style>
